20aa3a5fc7d97b747368fbad6f5afc5c7d7995a6
govind <govind.gupta2051@gmail.com>
2020-09-27 20:45:00 +0530

    Inheritance

---
+++ Inheritance/src/Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+    private final double radius;
+
+    public Circle(double radius) {
+        if (radius < 0) {
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return getRadius() * getRadius() * Math.PI;
+    }
+}
